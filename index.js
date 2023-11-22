37d93bd4606cb11f72ac06148f9b05fef2bd0945
kingbri <bdashore3@proton.me>
2023-11-21 21:48:02 -0500

    Extension: Add basic functionality
    Add a popup and HTML loading.
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- index.js
+++ index.js
@@ -5,59 +5,69 @@
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 
 //You'll likely need to import some other functions from the main script
-import { saveSettingsDebounced } from "../../../../script.js";
+import { callPopup, getRequestHeaders, saveSettingsDebounced } from "../../../../script.js";
+import { isTabby } from "../../../textgen-settings.js";
+import { findSecret, readSecretState } from "../../../secrets.js";
 
 // Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+const extensionName = "tabby-loader";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};
-
-
  
 // Loads the extension settings if they exist, otherwise initializes them to the defaults.
 async function loadSettings() {
-  //Create the settings if they don't exist
-  extension_settings[extensionName] = extension_settings[extensionName] || {};
-  if (Object.keys(extension_settings[extensionName]).length === 0) {
-    Object.assign(extension_settings[extensionName], defaultSettings);
-  }
-
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+    //Create the settings if they don't exist
+    extension_settings[extensionName] = extension_settings[extensionName] || {};
+  
+    if (Object.keys(extension_settings[extensionName]).length === 0) {
+      Object.assign(extension_settings[extensionName], defaultSettings);
+    }
+  
+    // Updating settings in the UI
+    $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
 }
 
 // This function is called when the extension settings are changed in the UI
 function onExampleInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].example_setting = value;
-  saveSettingsDebounced();
+    const value = Boolean($(event.target).prop("checked"));
+    extension_settings[extensionName].example_setting = value;
+    saveSettingsDebounced();
 }
 
+// const popupResult = await callPopup(editorHtml, "confirm", undefined, { okButton: "Save" });
+
 // This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
+async function onLoaderOpenClick() {
+    if (!isTabby()) {
+        toastr.error("This function is only useable when TabbyAPI is selected. Please select TabbyAPI as your SillyTavern API.");
+
+        return;
+    }
+
+    console.log(await findSecret("admin_key_tabby"))
+
+    const loaderHtml = $.get(`${extensionFolderPath}/loader.html`);
+
+    const popupResult = await callPopup(loaderHtml, "confirm", undefined, { okButton: "Ok" });
 }
 
 // This function is called when the extension is loaded
 jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+    // This is an example of loading HTML from a file
+    const settingsHtml = await $.get(`${extensionFolderPath}/dropdown.html`);
 
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
-  $("#extensions_settings").append(settingsHtml);
+    // Append settingsHtml to extensions_settings
+    // extension_settings and extensions_settings2 are the left and right columns of the settings menu
+    // Left should be extensions that deal with system functions and right should be visual/UI related 
+    $("#extensions_settings").append(settingsHtml);
 
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
+    // These are examples of listening for events
+    $("#load_model_button").on("click", function () {
+        onLoaderOpenClick()
+    });
+    $("#example_setting").on("input", onExampleInput);
 
-  // Load settings when starting things up (if you have any)
-  loadSettings();
+    // Load settings when starting things up (if you have any)
+    loadSettings();
 });
